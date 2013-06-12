20540bf7e73f3d71296fe8b3d81ced967a2ed64e
vagrant <vagrant@precise64.(none)>
2013-06-12 11:01:13 +0000

    learn c the hard way

---
+++ Makefile
@@ -0,0 +1,6 @@
+CFLAGS=-Wall -g
+
+all: ex7
+
+clean:
+	rm -rf ex5 ex6 ex7 broken
---
+++ broken.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/* Warning: This programme is broken on purpose
+ * So, like... don't use it.
+ */
+
+int main()
+{
+    int age = 10;
+    int height;
+
+    printf("I am %d years old.\n");
+    printf("I am %d inches tall.\n", height);
+
+    return 0;
+}
+
